f441f5c53fffd5d84dea1e0d70cfece58c6bebbb
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-01-12 16:11:43 +0700

    chore: init `filament` :fire:
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- resources/types.d.ts
+++ resources/types.d.ts
@@ -11,10 +11,3 @@ interface ImportMetaEnv {
   SENTRY_DSN?: string
   SENTRY_PROFILING_ENABLE?: boolean
 }
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
