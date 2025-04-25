2f5a3e79e663bf0587f9eb94cd8c08828fbd29ff
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-04-25 13:03:52 +0700

    test(continent): add managers and athletes tests
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/Feature/Pages/Continent/EditTest.php
+++ tests/Feature/Pages/Continent/EditTest.php
@@ -3,7 +3,11 @@
 declare(strict_types=1);
 
 use App\Filament\Resources\ContinentResource\Pages\EditContinent;
+use App\Filament\Resources\ContinentResource\RelationManagers\AthletesRelationManager;
+use App\Filament\Resources\ContinentResource\RelationManagers\ManagersRelationManager;
+use App\Models\Classification;
 use App\Models\Continent;
+use Filament\Tables\Actions\EditAction;
 
 use function Pest\Livewire\livewire;
 
@@ -22,4 +26,36 @@ it('can edit a record', function () {
 
     expect($record->refresh())
         ->name->toBe('Updated');
+});
+
+it('can manage managers', function () {
+    $record = Continent::factory()
+        ->withManagers(5)
+        ->createOne();
+
+    $page = livewire(ManagersRelationManager::class, [
+        'ownerRecord' => $record,
+        'pageClass' => EditContinent::class,
+    ])->assertOk();
+
+    $page->callTableAction(EditAction::class, $record->managers->first(), [
+        'name' => 'Updated',
+    ])->assertHasNoTableActionErrors();
+});
+
+it('can manage athletes', function () {
+    $record = Continent::factory()
+        ->withAthletes(5, fn () => [
+            'class_id' => Classification::factory(),
+        ])
+        ->createOne();
+
+    $page = livewire(AthletesRelationManager::class, [
+        'ownerRecord' => $record,
+        'pageClass' => EditContinent::class,
+    ])->assertOk();
+
+    $page->callTableAction(EditAction::class, $record->athletes->first(), [
+        'name' => 'Updated',
+    ])->assertHasNoTableActionErrors();
 });
--- tests/Feature/Pages/Continent/ListTest.php
+++ tests/Feature/Pages/Continent/ListTest.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use App\Filament\Resources\ContinentResource\Pages\ListContinents;
+use App\Filament\Resources\ContinentResource\RelationManagers\AthletesRelationManager;
+use App\Filament\Resources\ContinentResource\RelationManagers\ManagersRelationManager;
+use App\Models\Classification;
 use App\Models\Continent;
 
 use function Pest\Livewire\livewire;
@@ -13,4 +16,34 @@ it('can show all records', function () {
     $page = livewire(ListContinents::class);
 
     $page->assertCanSeeTableRecords($records);
+});
+
+it('can have many managers', function () {
+    $record = Continent::factory()
+        ->withManagers(5)
+        ->createOne();
+
+    $page = livewire(ManagersRelationManager::class, [
+        'ownerRecord' => $record,
+        'pageClass' => ListContinents::class,
+    ]);
+
+    $page->assertOk()
+        ->assertCanSeeTableRecords($record->managers);
+});
+
+it('can have many athletes', function () {
+    $record = Continent::factory()
+        ->withAthletes(5, fn () => [
+            'class_id' => Classification::factory(),
+        ])
+        ->createOne();
+
+    $page = livewire(AthletesRelationManager::class, [
+        'ownerRecord' => $record,
+        'pageClass' => ListContinents::class,
+    ]);
+
+    $page->assertOk()
+        ->assertCanSeeTableRecords($record->athletes);
 });
