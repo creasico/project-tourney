616fb631c3322e8d2aadc29cbe93210f8e47f15a
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-05-01 03:25:50 +0700

    chore: add missing file for b8879ec
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->dontReport([
-            UnprocessableMatchupException::class
+            UnprocessableMatchupException::class,
         ]);
     })->create();
