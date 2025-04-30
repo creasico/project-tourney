2bd3bfde7914c081f096e61456c36f24c8df9d0d
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-04-30 20:30:52 +0700

    chore: ignore coverage on `gate` method in `HorizonServiceProvider`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -24,6 +24,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      * Register the Horizon gate.
      *
      * This gate determines who can access Horizon in non-local environments.
+     *
+     * @codeCoverageIgnore
      */
     protected function gate(): void
     {
