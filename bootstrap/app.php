b8879ecc9a3d73275227cda7d7ed12dd60573aca
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-04-30 12:33:28 +0700

    chore: prevent duplicate reports for `UnprocessableMatchupException`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Exceptions\UnprocessableMatchupException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,5 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->dontReport([
+            UnprocessableMatchupException::class
+        ]);
     })->create();
