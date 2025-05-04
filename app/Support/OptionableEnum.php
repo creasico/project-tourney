706077d24ca8ce4c683485e2905093dc0ae0c1f3
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-05-04 22:31:35 +0700

    feat: add ability to recieve arbitrary label value on `OptionableEnum`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/Support/OptionableEnum.php
+++ app/Support/OptionableEnum.php
@@ -9,6 +9,8 @@ namespace App\Support;
  */
 trait OptionableEnum
 {
+    private static array $labelsCache = [];
+
     public static function toOptions(): array
     {
         $out = [];
@@ -18,5 +20,33 @@ trait OptionableEnum
         }
 
         return $out;
+    }
+
+    public static function fromLabel(string $label): ?static
+    {
+        if (array_key_exists($label, self::$labelsCache)) {
+            return self::$labelsCache[$label];
+        }
+
+        foreach (self::cases() as $case) {
+            $label = str($label)->lower();
+
+            if (
+                method_exists($case, 'label') &&
+                $label->is($case->label(), true)
+            ) {
+                return self::$labelsCache[$label->toString()] = $case;
+            }
+
+            if ($label->is($case->name, true)) {
+                return self::$labelsCache[$label->toString()] = $case;
+            }
+        }
+
+        try {
+            return self::tryFrom($label);
+        } catch (\Throwable $er) {
+            return null;
+        }
     }
 }
