1ff874de51c72c8f10bb6640098cdf4fbf43bbcf
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-05-04 22:36:28 +0700

    chore: fix issue caused by 706077d
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/Support/OptionableEnum.php
+++ app/Support/OptionableEnum.php
@@ -9,8 +9,6 @@ namespace App\Support;
  */
 trait OptionableEnum
 {
-    private static array $labelsCache = [];
-
     public static function toOptions(): array
     {
         $out = [];
@@ -24,10 +22,6 @@ trait OptionableEnum
 
     public static function fromLabel(string $label): ?static
     {
-        if (array_key_exists($label, self::$labelsCache)) {
-            return self::$labelsCache[$label];
-        }
-
         foreach (self::cases() as $case) {
             $label = str($label)->lower();
 
@@ -35,11 +29,11 @@ trait OptionableEnum
                 method_exists($case, 'label') &&
                 $label->is($case->label(), true)
             ) {
-                return self::$labelsCache[$label->toString()] = $case;
+                return $case;
             }
 
             if ($label->is($case->name, true)) {
-                return self::$labelsCache[$label->toString()] = $case;
+                return $case;
             }
         }
 
